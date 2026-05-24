d1ace4c419f1f789b9512c45bd2af3aa9a5704d3
Erik Dubois <erik.dubois@gmail.com>
2026-05-24 12:08:50 +0200

    update

--- fix-icon-cache.sh
+++ fix-icon-cache.sh
@@ -19,7 +19,7 @@ for file in "${files[@]}"; do
     find / -type f -name "$file" -exec rm -f {} \; 2>/dev/null
 done
 
-file="/home/erik/DATA/EDU/neo-candy-icons/usr/share/icons/or-beautyline/devices/scalable/battery-level-100-symbolic.symbolic (copy 1).svg"
+file="/home/erik/EDU/neo-candy-icons/usr/share/icons/or-beautyline/devices/scalable/battery-level-100-symbolic.symbolic (copy 1).svg"
 
 if [ -f "$file" ]; then
     rm -v "$file"
@@ -44,11 +44,11 @@ echo "End of symbolic link check"
 
 sleep 5
 
-echo "Searching for hidden files in /home/erik/DATA/EDU/neo-candy-icons/usr/share/icons/"
-find /home/erik/DATA/EDU/neo-candy-icons/usr/share/icons/ -type f -iname ".*" -ls
+echo "Searching for hidden files in /home/erik/EDU/neo-candy-icons/usr/share/icons/"
+find /home/erik/EDU/neo-candy-icons/usr/share/icons/ -type f -iname ".*" -ls
 
-echo "Searching for hidden directories in /home/erik/DATA/EDU/neo-candy-icons/usr/share/icons/"
-find /home/erik/DATA/EDU/neo-candy-icons/usr/share/icons/ -type d -iname ".*" -ls
+echo "Searching for hidden directories in /home/erik/EDU/neo-candy-icons/usr/share/icons/"
+find /home/erik/EDU/neo-candy-icons/usr/share/icons/ -type d -iname ".*" -ls
 
 sleep 10
 
